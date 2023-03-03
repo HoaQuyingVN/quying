db11f47b4aa37b4c5aa927abdf5061534c2a511b
HoaQuyingVN <126839568+HoaQuyingVN@users.noreply.github.com>
2023-03-03 14:40:26 +0700

    Create main.py

---
+++ cli/quying/cli/main.py
@@ -0,0 +1,35 @@
+import logging
+import argparse
+
+log = logging.getLogger(__name__)
+
+
+def main(args=None):
+
+    parser = argparse.ArgumentParser(prog='greet', description='Command line interface for the greet package')
+    parser.add_argument(
+        '--loglevel', default='info', help='Log level',
+        choices=['debug', 'info', 'warning', 'error', 'critical'],
+    )
+    subparsers = parser.add_subparsers(help='Sub-commands')
+
+    
+
+    # Parse all command line arguments
+    args = parser.parse_args(args)
+
+    # This is not a good way to handle the cases
+    # where help should be printed.
+    # TODO: there must be a better way?
+    if hasattr(args, 'func'):
+        # Call the desired subcommand function
+        logging.basicConfig(level=args.loglevel.upper())
+        args.func(args)
+        return 0
+    else:
+        parser.print_help()
+        return 0
+
+    # log.debug('some debug')
+    # log.info('some info')
+    # log.warning('some warning')
